5832c913b81ef31c6c6ee846606e5856e91fe938
aman.nigam26 <aman.nigam26@gmail.com>
2018-08-31 19:08:28 +0530

    to implement dislike and likes

--- src/app/bot-home-screen/bot-home-screen.component.ts
+++ src/app/bot-home-screen/bot-home-screen.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
 import {ServerService} from '../server.service';
 import {stringDistance} from 'codelyzer/util/utils';
+import {assertNumber} from '@angular/core/src/render3/assert';
 
 @Component({
   selector: 'app-bot-home-screen',
@@ -12,6 +13,7 @@ export class BotHomeScreenComponent implements OnInit {
   showData = [];
   /*@Output() answerAdded = new EventEmitter<{ansContent: string}>();*/
   answers = [{content: 'just first answer test'}];
+  likeInfo = {id: String , hits: Number};
 
   constructor(private serverService: ServerService) {
   }
@@ -19,14 +21,14 @@ export class BotHomeScreenComponent implements OnInit {
   ngOnInit() {
   }
 
-  onGetServerSearch (value) {
+  onGetServerSearch(value) {
     this.serverService.getServer(value)
       .subscribe(
         (response) => {
           const jsonData = response.json();
           this.changeCurrent(jsonData);
-          },
-              (error) => console.log(error)
+        },
+        (error) => console.log(error)
       );
   }
 
@@ -36,9 +38,30 @@ export class BotHomeScreenComponent implements OnInit {
 
   }
 
- /* answerAdded (c) {
-    this.answers.push({
-      content: c
-  });*/
+  sendLike(id) {
+    this.likeInfo.id  = id;
+    this.likeInfo.hits = 1;
+    this.serverService.putLike(this.likeInfo)
+      .subscribe(
+        (response) => {
+          const value = response.json();
+          console.log(value);
+        },
+        (error) => console.log(error)
+      );
+  }
+
+  sendDisLike(id) {
+    this.likeInfo.id  = id;
+    this.likeInfo.hits = 1;
+    this.serverService.putDisLike(this.likeInfo)
+      .subscribe(
+        (response) => {
+          const value = response.json();
+          console.log(value);
+        },
+        (error) => console.log(error)
+      );
+  }
 
 }
--- src/app/server.service.ts
+++ src/app/server.service.ts
@@ -11,4 +11,12 @@ export class ServerService {
   getServer(value) {
     return this.http.get("http://10.204.253.41:8080/allianz-bot/search/document?q=" + value);
   }
+
+  putLike(value) {
+    return this.http.put("http://10.204.253.41:8080/allianz-bot/update/document", value);
+  }
+
+  putDisLike(value) {
+    return this.http.put("http://10.204.253.41:8080/allianz-bot/update/document", value);
+  }
 }
